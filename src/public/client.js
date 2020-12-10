1f6b60fbad6225a04f523e79ae38412c6471e31e
Joey Berger <44282450+joeyBerger@users.noreply.github.com>
2020-12-10 10:43:13 +0200

    tabs, carousel working

--- src/public/client.js
+++ src/public/client.js
@@ -1,43 +1,157 @@
-let store = {
-    user: { name: "Student" },
-    apod: '',
+//Udacity example
+// let store = {
+//     user: { name: "Student" },
+//     apod: '',
+//     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
+// }
+
+// const updateStore = (store, newState) => {
+//     console.log(newState)
+//     store = Object.assign(store, newState)
+//     render(root, store)
+// }
+//Udacity example
+
+//my code
+let store = Immutable.Map({
+    user: Immutable.Map({ 
+        name: "Student",
+    }),
+    apod: Immutable.Map({
+        image: Immutable.Map({
+            date: '',
+            explanation: '',
+            media_type: '',
+            service_version: '',
+            title: '',
+            url: '',
+        }),
+    }),
+    roverInfo: Immutable.List([Immutable.Map({
+        name: '',
+        landing_date: '',
+        launch_date: '',
+        status: '',
+        photos: Immutable.List(Immutable.Map({
+            img_src: '',
+            date : '',
+        }))
+    })]),
+    currentTab: 'Curiosity',
     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
+    currentTab : 'Curiosity',
+})
+
+const updateStore = (state, newState) => {
+    store = state.merge(newState)
+    render(root, store)
 }
+//my code
+
 
 // add our markup to the page
 const root = document.getElementById('root')
-
-const updateStore = (store, newState) => {
-    console.log('in update store')
-    store = Object.assign(store, newState)
-    render(root, store)
-}
 
 const render = async (root, state) => {
     root.innerHTML = App(state)
 }
 
+const onClick = (tabName) => {
+    const currentTab = tabName.id;
+    updateStore(store,{currentTab})
+}
+
+const returnInnerTabHTML = (name) => {
+    return (`
+        <li class="nav-item">
+            <a class="nav-link active" href="#" onclick="onClick(this)" id="${name}">${name}</a>
+        </li>
+    `)
+}
+
+const displayTabs = (rovers) => {
+    return (`
+        <ul class="nav nav-tabs">
+        ${rovers.map(rover => returnInnerTabHTML(rover)).join('')}
+        </ul>
+    `)
+}
+
+const returnCarouselListItem = (idx) => {
+    return(`
+        <li data-target="#carouselExampleIndicators" data-slide-to="${idx}"></li>
+    `)
+}
+
+const returnCarouselPhoto = (photo,idx) => {
+    return(`
+        <div class="carousel-item${idx===0?' active':''}">
+            <img class="d-block w-100" src=${photo.img_src} alt=${photo.date}>
+        </div>
+    `)
+}
+
+const returnCarousel = (photos) => {
+    return(`
+           <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+            ${photos.map((photo,i) => returnCarouselListItem(i)).join('')}
+            </ol>
+            <div class="carousel-inner">
+            ${photos.map((photo,i) => returnCarouselPhoto(photo,i)).join('')}
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+            </a>
+        </div>        
+    `)
+}
+
+const displayRoverInfo = (whcRover,roverInfo) => {
+    let rover = roverInfo.find(info => info['name'] === whcRover);
+    console.log('in displayRoverInfo',roverInfo)
+    if (rover) {
+        return (
+            `
+            <div>Name: ${rover.name}</div>
+            <div>Launch Date: ${rover.launch_date}</div>
+            <div>Landing Date: ${rover.landing_date}</div>
+            <div>Status: ${rover.status}</div>
+            ${returnCarousel(rover.photos)}           
+            `
+        )
+    } else {
+        fetch(`http://localhost:3000/rover/${whcRover}`) 
+            .then(res => res.json())
+            .then((roverInfo) => {
+                let newStore = store.toJS()
+                newStore.roverInfo.push(roverInfo);
+                updateStore(store, newStore)
+            })
+            .catch(err => console.log(err))
+        return `Loading...`
+    }
+}
 
 // create content
 const App = (state) => {
-    let { rovers, apod } = state
+    // let { rovers, apod } = state
+
+    let { rovers, apod, user, roverInfo, currentTab } = state.toJS();
+    console.log(state)
 
     return `
         <header></header>
         <main>
-            ${Greeting(store.user.name)}
+            ${Greeting(user.name)}
             <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Butt Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
+                ${displayTabs(rovers)}
+                ${displayRoverInfo(currentTab,roverInfo)}                
             </section>
         </main>
         <footer></footer>
@@ -46,6 +160,40 @@ const App = (state) => {
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
+    // store.toJS().rovers.forEach((rover) => {
+    //     var element = document.getElementById(rover.toLowerCase());
+    //     element.onclick = () => {console.log(rover)}
+    // })
+
+    // var tag = document.createElement("ul");
+    // var text = document.createListNode("Tutorix is the best e-learning platform");
+    // tag.appendChild(text);
+    // var element = document.getElementById("new");
+    // element.appendChild(tag);
+
+    // var list = document.createElement("UL");
+    // list.setAttribute('class', 'nav nav-tabs');
+    // var listItem = document.createElement("LI");
+    // listItem.setAttribute('class', 'nav-item');
+    // var hyperlink = document.createElement("A");
+    // hyperlink.setAttribute('class', 'nav-link active');
+    // hyperlink.setAttribute('href', '#');
+    // hyperlink.innerHTML = "New text!";
+    // listItem.appendChild(hyperlink);
+    // list.appendChild(listItem);
+
+    // var p = document.createElement('p');
+    // p.innerHTML = "New text!";
+    // root.appendChild(p);
+
+
+
+
+    // var textnode=document.createTextNode(firstname);
+    // node.appendChild(textnode);
+    // document.getElementById("demo").appendChild(node);
+    // root.appendChild(list);
+
     render(root, store)
 })
 
@@ -58,7 +206,6 @@ const Greeting = (name) => {
             <h1>Welcome, ${name}!</h1>
         `
     }
-
     return `
         <h1>Hello!</h1>
     `
@@ -70,19 +217,49 @@ const ImageOfTheDay = (apod) => {
     const today = new Date()
     const photodate = new Date(apod.date)
 
-    if (!apod || apod.image.date === today.getDate() ) {
+    // if (!apod || apod.image.date === today.getDate() ) {
+    if (apod.image.date === '' || apod.image.date === today.getDate() ) {        
         getImageOfTheDay(store)
     }
 
+    // return(
+    //     `   <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    //         <ol class="carousel-indicators">
+    //         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    //         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    //         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    //         </ol>
+    //         <div class="carousel-inner">
+    //         <div class="carousel-item active">
+    //             <img class="d-block w-100" src="https://i.pinimg.com/originals/54/7a/9c/547a9cc6b93e10261f1dd8a8af474e03.jpg" alt="First slide">
+    //         </div>
+    //         <div class="carousel-item">
+    //             <img class="d-block w-100" src="https://i.pinimg.com/originals/54/7a/9c/547a9cc6b93e10261f1dd8a8af474e03.jpg" alt="Second slide">
+    //         </div>
+    //         <div class="carousel-item">
+    //             <img class="d-block w-100" src="https://wonderfulengineering.com/wp-content/uploads/2016/01/cool-wallpaper-6.jpg" alt="Third slide">
+    //         </div>
+    //         </div>
+    //         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    //         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    //         <span class="sr-only">Previous</span>
+    //         </a>
+    //         <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    //         <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    //         <span class="sr-only">Next</span>
+    //         </a>
+    //     </div>
+    //     `
+    // )
+
     // check if the photo of the day is actually type video!
-    if (apod.image.media_type === "video") {
-        console.log('got a video')
+    if (apod.image && apod.image.media_type === "video") {
         return (`
             <p>See today's featured video <a href="${apod.image.url}">here</a></p>
             <p>${apod.image.title}</p>
             <p>${apod.image.explanation}</p>
         `)
-    } else {
+    } else if (apod.image) {
         return (`
             <img src="${apod.image.url}" height="350px" width="100%" />
             <p>${apod.image.explanation}</p>
@@ -94,25 +271,14 @@ const ImageOfTheDay = (apod) => {
 
 // Example API call
 const getImageOfTheDay = (state) => {
+    return ''
     let { apod } = state
-
-    fetch(`http://localhost:3000/apod`)
+    console.log('getImageOfTheDay')
+    // fetch(`http://localhost:3000/apod`)
+    fetch(`http://localhost:3000/rover/Curiosity`)
         .then(res => res.json())
         .then(apod => updateStore(store, { apod }))
-
-    // fetch(`http://localhost:3000/apod`)
-    // .then(res => res.json())
-    // .then((apod) => {
-    //     console.log(apod);
-    //     updateStore(store, { apod })
-    // })
-
-    var map1 = Immutable.Map({ a: 1, b: 2, c: 3 });
-    var map2 = map1.set('b', 50);
-    map1.get('b'); // 2
-    map2.get('b'); // 50
-
-    console.log(map1)
+        .catch(err => console.log(err))
 
     // return data
 }
--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
 const path = require('path')
-
+// const request = require('request');
 const app = express()
 const port = 3000
 
@@ -18,15 +18,46 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 app.get('/apod', async (req, res) => {
     console.log('in get request')
     try {
-        console.log('process.env.API_KEY',process.env.API_KEY)
         let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-        
-            // .then((res) => {console.log(res.json()); return res})
             .then(res => res.json())
         res.send({ image })
     } catch (err) {
         console.log('error:', err);
     }
 })
+
+app.get('/rover/:id', async (req, res) => {
+    try {
+        let roverInfoObj = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${req.params.id}/?api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        //need to gaurd against error
+        const maxSol = roverInfoObj.photo_manifest.max_sol;
+        // console.log("maxSol",maxSol);
+
+        let photos = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${req.params.id}/photos?sol=${maxSol}&api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        //need to gaurd against error
+
+        // photos = photos.photos.map(photo => photo.id).sort((a, b) => b - a).slice(0,10);
+        photos = photos.photos.sort((a, b) => b.id - a.id)
+            .map((photoInfo) => {return {img_src : photoInfo.img_src, date: photoInfo.earth_date}})
+            .slice(0,10);       
+
+        returnObj = {
+            name: roverInfoObj.photo_manifest.name,
+            landing_date: roverInfoObj.photo_manifest.landing_date,
+            launch_date: roverInfoObj.photo_manifest.launch_date,
+            status: roverInfoObj.photo_manifest.status,
+            photos: photos,
+        }
+        console.log(returnObj)
+        res.send( returnObj )
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
+
+//https://api.nasa.gov/mars-photos/api/v1/manifests/Curiosity/?api_key=kPeWfUpnhB321Wq2i7GJgRuLn4nGAKxO0G9VSUM1
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
